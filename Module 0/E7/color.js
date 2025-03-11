7d400ecffb41fe5982e56943b268b03c591bc31e
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2025-03-11 17:00:45 +0100

    Kleuren_Cascade

---
+++ Module 0/E7/color.js
@@ -0,0 +1,11 @@
+document.getElementById("startButton").addEventListener("click", () => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = "red";
+        setTimeout(() => {
+            document.body.style.backgroundColor = "green";
+            setTimeout(() => {
+                document.body.style.backgroundColor = "blue";
+            }, 1000);
+        }, 1000);
+    }, 1000);
+});
